960b05a5a12e9df0fb1a1766a9f93158d67f1a86
Jettsyy <teazyyy2512@gmail.com>
2025-05-16 14:58:52 +0700

    feat: base core type

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router";
+import Home from "./components/Home";
+import CoreType from "./components/CoreType";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/core-type" element={<CoreType/>} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CoreType.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+
+const CoreType = () => {
+  // string
+  const name: string = "John Doe";
+  // number
+  const age: number = 30;
+  // boolean
+  const isStudent: boolean = false;
+  // any
+  const variableAny: any = "Hello";
+  // Tuple
+  const variableTuple: [string, number] = ["John", 30];
+  // Array
+  const variableArr: number[] = [1, 2, 3, 4, 5];
+  const favoriteFoods: string[] = ["noddle", "pizza", "rice"];
+
+  // Enum
+  enum Role {
+    ADMIN = "ADMIN",
+    USER = "USER",
+  }
+  const role: Role = Role.ADMIN;
+
+  // literal type
+  // Chỉ cho phép biến có giá trị là "Hello" hoặc "Hi"
+  type Greeting = "Hello" | "Hi";
+  const greeting: Greeting = "Hello";
+
+  // union type
+  // Chỉ cho phép biến có kiểu dữ liệu là string hoặc number
+  type ProductIdType = string | number;
+  const productId: ProductIdType = "12345";
+
+  // unknown type
+  // unknown là một kiểu dữ liệu an toàn hơn any
+  // unknown không thể xác định được kiểu dữ liệu của nó
+  const print = (value: unknown) => {
+    // Unknown phải check kiểu dữ liệu trước khi sử dụng
+    // any thì không cần check
+    if (typeof value === "string" ) {
+      console.log(value.toLowerCase());
+    }
+  };
+  print("Hello");
+
+
+  return (
+    <>
+      <div>
+        {" "}
+        {name} {age} {isStudent} {variableAny} {greeting} {productId}
+      </div>
+      <div>
+        {" "}
+        {variableTuple[0]} {variableTuple[1]}
+      </div>
+      <h2> Món Ăn Yêu Thích</h2>
+      <ul>
+        {favoriteFoods.map((food) => (
+          <li>{food}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default CoreType;
