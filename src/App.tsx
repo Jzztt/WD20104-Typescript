b07e8781cecc4e95516207e72c4d6b9483c7c024
Jettsyy <teazyyy2512@gmail.com>
2025-05-16 16:06:13 +0700

    feat: base client layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Route, Routes } from "react-router";
 import Home from "./components/Home";
 import CoreType from "./components/CoreType";
+import ListProduct from "./components/ListProduct";
+import FormCreateProduct from "./components/FormCreateProduct";
+import ClientLayout from "./Layouts/ClientLayout";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/core-type" element={<CoreType/>} />
+        <Route path="/" element={<ClientLayout />}>
+          <Route index element={<Home />} />
+          <Route path="/core-type" element={<CoreType />} />
+          <Route path="/products" element={<ListProduct />} />
+          <Route path="/create-product" element={<FormCreateProduct />} />
+        </Route>
       </Routes>
     </>
   );
