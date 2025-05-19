c033ace2d4e58a094a0af0bc9b5aa5925da744b9
Jettsyy <teazyyy2512@gmail.com>
2025-05-19 16:08:55 +0700

    feat: show product

--- src/components/ListProduct.tsx
+++ src/components/ListProduct.tsx
@@ -1,10 +1,67 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
+interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  category: string;
+  description: string;
+}
 const ListProduct = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+  const fetchProduct = async () => {
+    const fetchProductResponse = await axios.get(
+      "http://localhost:3000/products"
+    );
+    setProducts(fetchProductResponse.data);
+  };
+  useEffect(() => {
+    fetchProduct();
+  },[]);
 
   return (
-    <div>ListProduct</div>
-  )
-}
+    <>
+      <div className="relative overflow-x-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Product name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Color
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map((product) => (
+              <tr
+                key={product.id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+              >
+                <th
+                  scope="row"
+                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {product.name}
+                </th>
+                <td className="px-6 py-4"> {product.description}</td>
+                <td className="px-6 py-4"> {product.category}</td>
+                <td className="px-6 py-4"> {product.price}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
 
-export default ListProduct
+export default ListProduct;
