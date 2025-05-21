124cefb873baf47f258ddb316f09f07930497c1a
Jettsyy <teazyyy2512@gmail.com>
2025-05-21 14:48:31 +0700

    feat: UI for form create product

--- src/components/FormCreateProduct.tsx
+++ src/components/FormCreateProduct.tsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
 
 const FormCreateProduct = () => {
+  // Chức năng thêm sản phẩm mới
+  // 1. Tạo một form để nhập thông tin sản phẩm mới
+  // 2. Lấy dữ liệu từ form
+  // 3. Gửi dữ liệu đến API để thêm sản phẩm mới
   return (
-    <div>FormCreateProduct</div>
-  )
-}
+    <>
+      <div className="w-[500px] mx-auto mt-10">
+        <h1 className="text-2xl font-bold mb-5 text-center">Create Product</h1>
+        <form action="">
+          <div className="mb-5">
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Product Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="price"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Price
+            </label>
+            <input
+              type="text"
+              id="price"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="Description"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Description
+            </label>
+            <textarea
+              id="Description"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="Category"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Category
+            </label>
+            <select
+              id="countries"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option selected>Choose a Category</option>
+              <option value="Laptop">Laptop</option>
+              <option value="SmartPhone">SmartPhone</option>
+            </select>
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default FormCreateProduct
+export default FormCreateProduct;
